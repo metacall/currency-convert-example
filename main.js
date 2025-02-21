a67ec413e33e53b4a69cf26422c9143629f252c9
Rohan Kumar Mahato <rohankumarmahato345@gmail.com>
2025-02-21 07:17:24 +0530

    updated_mainJS

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-import { convertCurrency } from "./main.py";
+const { convertCurrency } = require( "./main.py" );
 console.log(convertCurrency(100, "USD", "INR")); // It will Show the conversion of 100 USD to INR
